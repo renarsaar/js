0e8cf8257d2b6e4f291c1147f6ef9f0876a851c6
renarsaar <renar.saaremets@gmail.com>
2020-05-04 18:24:55 +0300

    Breakout UI

---
+++ Breakout/main.js
@@ -0,0 +1,9 @@
+// Ui vars
+const rulesBtn = document.getElementById("rules-btn");
+const closeBtn = document.getElementById("close-btn");
+const rules = document.getElementById("rules");
+
+// Rules & Close rules
+rulesBtn.addEventListener("click", () => rules.classList.add("show"));
+
+closeBtn.addEventListener("click", () => rules.classList.remove("show"));
