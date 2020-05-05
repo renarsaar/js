5b0e58a14e55e50967f066aa7b2ebdc895ef6b30
renarsaar <renar.saaremets@gmail.com>
2020-05-05 11:00:35 +0300

    9x5 Bricks

--- Breakout/main.js
+++ Breakout/main.js
@@ -7,6 +7,9 @@ const ctx = canvas.getContext("2d");
 
 let score = 0;
 
+const brickRowCount = 9;
+const brickColCount = 5;
+
 // Ball properties
 const ball = {
   x: canvas.width / 2,
@@ -27,6 +30,33 @@ const paddle = {
   dx: 0,
 };
 
+// Brick Properties
+const brickInfo = {
+  w: 70,
+  h: 20,
+  padding: 10,
+  offsetX: 45,
+  offsetY: 60,
+  visible: true,
+};
+
+const bricks = [];
+
+// Create bricks
+for (let i = 0; i < brickRowCount; i++) {
+  bricks[i] = [];
+  for (let j = 0; j < brickColCount; j++) {
+    const x = i * (brickInfo.w + brickInfo.padding) + brickInfo.offsetX;
+    const y = j * (brickInfo.h + brickInfo.padding) + brickInfo.offsetY;
+
+    bricks[i][j] = { x, y, ...brickInfo };
+  }
+}
+
+//
+console.log(bricks);
+//
+
 // Ball canvas
 function drawBall() {
   ctx.beginPath();
@@ -36,6 +66,7 @@ function drawBall() {
   ctx.closePath();
 }
 
+// Paddle canvas
 function drawPaddle() {
   ctx.beginPath();
   ctx.rect(paddle.x, paddle.y, paddle.w, paddle.h);
@@ -44,17 +75,31 @@ function drawPaddle() {
   ctx.closePath();
 }
 
-// Function to call canvas
-function draw() {
-  drawBall();
-  drawPaddle();
-  drawScore();
+// Brick Canvas
+function drawBricks() {
+  bricks.forEach((column) => {
+    column.forEach((brick) => {
+      ctx.beginPath();
+      ctx.rect(brick.x, brick.y, brick.w, brick.h);
+      ctx.fillStyle = brick.visible ? "#ff471a" : "transparent";
+      ctx.fill();
+      ctx.closePath();
+    });
+  });
 }
 
 // Score
 function drawScore() {
   ctx.font = "14px Arial";
   ctx.fillText(`Score: ${score}`, canvas.width - 70, 30);
+}
+
+// Call canvas
+function draw() {
+  drawBall();
+  drawPaddle();
+  drawScore();
+  drawBricks();
 }
 
 // Init()
